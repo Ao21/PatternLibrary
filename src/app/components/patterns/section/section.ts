e3f8eb4250024c1f8756dcd7f9d395605bf1bc09
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-08 10:44:23 +0000

    Create Section Page

--- src/app/components/patterns/section/section.ts
+++ src/app/components/patterns/section/section.ts
@@ -1,7 +1,9 @@
 import { Component, View } from 'angular2/angular2';
+import {BASE_COMPONENTS} from './../../base/base_modules.ts'
 import {SHARED_COMPONENTS} from '../../shared/shared_modules';
 import {HoverEdit, DRAGGING_DIRECTIVES} from '../../../directives/directives_modules';
-import {UIStore} from './../../../stores/stores_modules.ts'
+import {UIStore, SectionStore} from './../../../stores/stores_modules.ts'
+import {PatternService} from './../../../services/services_modules.ts';
 let template = require('./section.html');
 let styles = require('./section.scss');
 
@@ -11,10 +13,22 @@ let styles = require('./section.scss');
 @View({
 	template: template,
 	styles: [styles],
-	directives: [SHARED_COMPONENTS,DRAGGING_DIRECTIVES, HoverEdit]
+	directives: [SHARED_COMPONENTS,BASE_COMPONENTS,DRAGGING_DIRECTIVES, HoverEdit]
 })	    
 export class Section {
-	
+	fakeData: any;
+	sections: any;
+	sub: ISubscriptionDefinition<SectionStore>
+	constructor(
+		public sectionStore: SectionStore,
+		public patternService: PatternService
+	) {
+		this.fakeData = ['#8DE3FC', '#8DE3FC'];
+		this.sub = this.sectionStore.subscribe('section', state=> {
+			this.sections = state.get('components');
+		})
+		
+	}
 	
 	
 }
