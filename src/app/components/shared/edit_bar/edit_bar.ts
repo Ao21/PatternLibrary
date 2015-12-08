e5cb2f69c864595f34faef18bc5356745bbbce6d
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-08 10:43:28 +0000

    Update Edit Bar Component

--- src/app/components/shared/edit_bar/edit_bar.ts
+++ src/app/components/shared/edit_bar/edit_bar.ts
@@ -1,7 +1,12 @@
 import { Component, View } from 'angular2/angular2';
+import {AddPattern} from './../add_pattern/add_pattern.ts';
 let template = require('./edit_bar.html');
 let styles = require('./edit_bar.scss');
 
+/*
+	<edit-bar></edit-bar>
+	<any hover-edit></any>
+*/
 @Component({
 	selector: 'edit-bar',
 	host: {
@@ -11,6 +16,7 @@ let styles = require('./edit_bar.scss');
 @View({
 	template: template,
 	styles: [styles],
+	directives: [AddPattern]
 })	    
 export class EditBar {
 	isExtended: boolean = false;
