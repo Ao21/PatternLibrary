105bd59f147c428bac150df4b7355db04335724e
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-08 10:42:23 +0000

    Create Component Loader

---
+++ src/app/components/base/component_loader/component_loader.ts
@@ -0,0 +1,49 @@
+import { Component, View, OnInit, DynamicComponentLoader, Injector, provide, Renderer, ElementRef, ComponentRef } from 'angular2/angular2';
+import {ObservableWrapper, Promise, PromiseWrapper} from 'angular2/src/facade/async';
+import { Http } from 'angular2/http';
+
+import * as BaseModules from './../base_modules.ts';
+import {SHARED_COMPONENTS} from './../../shared/shared_modules.ts';
+let template = require('./component_loader.html');
+
+
+
+@Component({
+	selector: 'component-loader',
+	inputs: ['loader', 'data']
+})
+@View({
+	template: template,
+})
+export class ComponentLoader implements OnInit {
+	loader: string;
+	data: any;
+
+
+	constructor(
+		private _dComponentLoader: DynamicComponentLoader,
+		private _renderer: Renderer,
+		private _el: ElementRef,
+		public http: Http
+
+	) { }
+
+
+	onInit() {
+		let comp = _.find(SHARED_COMPONENTS, (e) => {
+			return e.name === this.loader;
+		})
+
+		if (!comp) {
+			return
+		}
+
+		return this._dComponentLoader.loadIntoLocation(comp, this._el, 'child')
+			.then((comp: any) => {
+				comp.instance.data = this.data;
+				comp.instance.init();
+			});
+
+	}
+
+}
