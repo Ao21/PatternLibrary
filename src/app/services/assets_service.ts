a9cfa8d8debf4e74021e180e4dc4d5cd78d4519e
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-11 16:25:43 +0000

    Updatess to Assets & Pattern Service

--- src/app/services/assets_service.ts
+++ src/app/services/assets_service.ts
@@ -1,42 +1,56 @@
 import { Inject, Injectable, Component} from 'angular2/angular2';
-
-import { Http, Headers } from 'angular2/http'; 
+import { Http, Headers } from 'angular2/http';
 
 @Injectable()
 export class AssetsService {
-	
+
 	url: string = 'http://localhost:8080/api/assets';
 	_assetsDict: any = [];
+	_cached: boolean = false;
 
 	constructor(public http: Http) {
-		this.generateAssets();	
-	}
-	
-	generateAssets() {
-		console.log(this._assetsDict);
-		if (!this._assetsDict || this._assetsDict.length === 0) {
-			this.http.get(this.url).subscribe(
-				res => {
-					console.log(res);
-				_.forEach(res.json(), (e) => {				
-					this._assetsDict[e.name] = e;
-				})
-				
-			}
-		);
-		}
 		
 	}
-	 	
-	get assets() {		
-		return this._assetsDict;
-	}
-	
-	getAsset(asset) {
-		this.generateAssets();
-		return this._assetsDict[asset];
+
+	generateAssets() {
+		return new Promise((resolve, rej) => {
+			if (this._cached===true) {
+				resolve(this._assetsDict)
+			} else {
+				this.http.get(this.url).subscribe(
+					res => {
+						_.forEach(res.json(), (e) => {
+							this._assetsDict[e.name] = e;
+						})
+						this._cached = true;
+						resolve(this._assetsDict);
+					})
+			}
+		})
+
 	}
 
-	
-	
+	get assets() {
+		return this._assetsDict;
+	}
+
+	getAsset(asset) {
+		return new Promise((resolve, rej) => {
+			if (this._cached===true) {
+				resolve(this._assetsDict[asset])
+			} else {
+				this.http.get(this.url).subscribe(
+					res => {
+						_.forEach(res.json(), (e) => {
+							this._assetsDict[e.name] = e;
+						})
+						resolve(this._assetsDict[asset]);
+					})
+			}
+		})
+
+	}
+
+
+
 }
