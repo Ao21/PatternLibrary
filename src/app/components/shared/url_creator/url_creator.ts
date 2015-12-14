77173e4ce670d4936d8f6e30aff807e5f3c88a3d
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-14 16:11:23 +0000

    Create a url creating input field

---
+++ src/app/components/shared/url_creator/url_creator.ts
@@ -0,0 +1,46 @@
+import { Component, View, Host, OnChanges, OnInit } from 'angular2/core';
+import {
+Control,
+ControlGroup,
+FORM_DIRECTIVES,
+NgFormModel,
+NgForm
+} from 'angular2/common';
+let template = require('./url_creator.html');
+
+@Component({
+	selector: 'url-creator',
+	inputs: ['controlPath: control','placeholder', 'value'],
+})
+@View({
+	template: template,
+	directives: [FORM_DIRECTIVES],
+	styles: [],
+})	    
+export class UrlCreatorInput implements OnInit, OnChanges {
+	formDir: NgFormModel;
+	placeholder: string = "";
+	controlPath: string;
+	control: any;
+	value: string;
+	
+	constructor(
+		 @Host() formDir: NgFormModel
+	) {
+		this.formDir = formDir;
+	}
+	
+	ngOnInit() {
+		console.log(this.controlPath);
+		this.control = this.formDir.form.controls[this.controlPath];
+		console.log(this.formDir.form.controls[this.controlPath]);		 
+	}
+	
+	ngOnChanges() {
+		if (this.value) {
+			this.value = this.value.trim().replace(/\s+/g, '-').toLowerCase();
+			this.control.updateValue(this.value);
+		}
+		
+	}
+}
