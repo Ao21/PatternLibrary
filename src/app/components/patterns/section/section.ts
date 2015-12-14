74472747f6d22bf320c2bff95b6e2ddebe441d8d
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-14 16:12:15 +0000

    Update Section Page to pull a section from the api or redirect

--- src/app/components/patterns/section/section.ts
+++ src/app/components/patterns/section/section.ts
@@ -1,10 +1,12 @@
-import { Component, View } from 'angular2/core';
+import { Component, View, Host } from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import {BASE_COMPONENTS} from './../../base/base_modules.ts'
 import {SHARED_COMPONENTS} from '../../shared/shared_modules';
 import {HoverEdit, DRAGGING_DIRECTIVES} from '../../../directives/directives_modules';
+import {ROUTER_DIRECTIVES,Router, RouteParams, CanActivate, OnActivate} from 'angular2/router';
+
 import {UIStore, SectionStore} from './../../../stores/stores_modules.ts'
-import {PatternService} from './../../../services/services_modules.ts';
+import {PatternService, SectionService} from './../../../services/services_modules.ts';
 let template = require('./section.html');
 let styles = require('./section.scss');
 
@@ -15,15 +17,28 @@ let styles = require('./section.scss');
 	template: template,
 	styles: [styles],
 	directives: [CORE_DIRECTIVES, SHARED_COMPONENTS,BASE_COMPONENTS,DRAGGING_DIRECTIVES, HoverEdit]
-})	    
+})	      
 export class Section {
 	fakeData: any;
 	sections: any;
+	section: any = {};
 	sub: ISubscriptionDefinition<SectionStore>
 	constructor(
+		@Host() public router: Router,
+		public routeParams: RouteParams,
+		public SectionService: SectionService,
 		public sectionStore: SectionStore,
 		public patternService: PatternService
 	) {
+		
+		this.SectionService.get(this.routeParams.params['url']).subscribe(
+			res => {
+				this.section = res.json();
+			},
+			err => {
+				this.router.parent.navigate(['./Import'])
+			}
+		)
 		this.fakeData = ['#8DE3FC', '#8DE3FC'];
 		this.sub = this.sectionStore.subscribe('section', state=> {
 			this.sections = state.get('components');
