31c5c226011231f600bdd1223ef95a696b513e1d
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-15 16:39:06 +0000

    Update Component Loader to include config data

--- src/app/components/base/component_loader/component_loader.ts
+++ src/app/components/base/component_loader/component_loader.ts
@@ -1,23 +1,27 @@
 import { Component, View, OnInit, DynamicComponentLoader, Injector, provide, Renderer, ElementRef, ComponentRef } from 'angular2/core';
 import {ObservableWrapper, Promise, PromiseWrapper} from 'angular2/src/facade/async';
 import { Http } from 'angular2/http';
+import {ComponentConfig} from './../../shared/component_config/component_config.ts';
 
 import * as BaseModules from './../base_modules.ts';
 import {SHARED_COMPONENTS} from './../../shared/shared_modules.ts';
 let template = require('./component_loader.html');
+let styles = require('./component_loader.scss');
 
 @Component({
 	selector: 'component-loader',
-	inputs: ['loader', 'data']
+	inputs: ['loader', 'data', 'config']
 })
     
 @View({
+	styles:[styles],    
 	template: template,
+	directives: [ComponentConfig]    
 })
 export class ComponentLoader implements OnInit {
 	loader: string;
 	data: any;
-
+	config: any;
 
 	constructor(
 		private _dComponentLoader: DynamicComponentLoader,
@@ -32,6 +36,7 @@ export class ComponentLoader implements OnInit {
 		let comp = _.find(SHARED_COMPONENTS, (e) => {
 			return e.name === this.loader;
 		})
+		
 
 		if (!comp) {
 			return
@@ -39,8 +44,11 @@ export class ComponentLoader implements OnInit {
 
 		return this._dComponentLoader.loadIntoLocation(comp, this._el, 'child')
 			.then((comp: any) => {
-				comp.instance.data = this.data;
-				comp.instance.init();
+				if (this.data) {
+					comp.instance.data = this.data;
+					comp.instance.init();
+				}
+				
 			});
 
 	}
