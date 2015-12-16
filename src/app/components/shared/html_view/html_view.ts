088867a881c40426a3bbc430fcef7edca20294c9
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-16 17:45:59 +0000

    Update HTMLView component to work with data from server

--- src/app/components/shared/html_view/html_view.ts
+++ src/app/components/shared/html_view/html_view.ts
@@ -1,6 +1,8 @@
 import { Component, View, ElementRef } from 'angular2/core';
 import { Tab, TabsGroup } from '../../base/base_modules';
 import {DOM} from 'angular2/src/platform/dom/dom_adapter';
+import {PatternService} from './../../../services/pattern_service.ts';
+import {Highlight} from './../../../pipes/highlight.ts';
 let template = require('./html_view.html');
 let styles = require('./html_view.scss');
 
@@ -14,13 +16,27 @@ let styles = require('./html_view.scss');
 @View({
 	template: template,
 	styles: [styles],
-	directives: [Tab, TabsGroup]
+	directives: [Tab, TabsGroup],
+	pipes: [Highlight]
 })
 export class HTMLView {
+	data: any;
+	tabs: any[] = [];
+
 	constructor(
-		private _el: ElementRef
-	) {}
+		private _el: ElementRef,
+		private patternService: PatternService
+	) { }
 	clickToCopy() {
 		//console.log(DOM.querySelector(this._el.nativeElement,'.content .selected code').innerText);
+	}
+
+	init() {
+		var pattern = this.patternService.patternsDict[this.data.HTMLViewData];
+		let angularData = pattern.data[0].angular.escaped.replace(/^[^&]*|[^gt;]*$/g, '').replace(/\t/g, '&nbsp;&nbsp;')
+		let htmlData = pattern.data[0].markup.escaped.replace(/^[^&]*|[^gt;]*$/g, '').replace(/\t/g, '&nbsp;&nbsp;')
+		this.tabs.push({ name: 'Angular', type: 'html', data: angularData })
+		this.tabs.push({ name: 'HTML', type: 'html', data: htmlData })
+		//hljs.highlightBlock('.hljs')
 	}
 }
