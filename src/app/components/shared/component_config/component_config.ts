4765796d21a877a414ae89c6c0adfe2f52dd3641
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-16 17:44:40 +0000

    Update the config component to work with data from API

--- src/app/components/shared/component_config/component_config.ts
+++ src/app/components/shared/component_config/component_config.ts
@@ -1,16 +1,55 @@
-import { Component, View } from 'angular2/core';
+import { Component, View, OnInit, ElementRef, Renderer } from 'angular2/core';
+import { ControlGroup, FormBuilder, NgForm, FORM_DIRECTIVES, NgFormModel, Validators} from 'angular2/common';
+import {FormComponent} from './../form_component/form_component.ts';
 import {DOM} from 'angular2/src/platform/dom/dom_adapter';
+import {SectionStore} from './../../../stores/section_store.ts';
 let template = require('./component_config.html');
 let styles = require('./component_config.scss');
 
 @Component({
 	selector: 'component-config',
-	inputs: ['data']
+	inputs: ['data', 'section']
 })
 @View({
 	template: template,
 	styles: [styles],
-})	    
-export class ComponentConfig {
-	
+	directives: [FormComponent]
+})
+export class ComponentConfig implements OnInit {
+	componentConfigForm: NgForm;
+	section: any;
+	form: ControlGroup;
+	data: any;
+	fields: any;
+	ctrl: any;
+
+	constructor(
+		public sectionStore: SectionStore,
+		public fb: FormBuilder,
+		public renderer: Renderer,
+		public el: ElementRef
+	) {
+		this.ctrl = [];
+
+	}
+
+	ngOnInit() {
+
+		this.fields = this.data;
+
+		_.forEach(this.fields, (e: any) => {
+			this.ctrl[e.control] = [''];
+		})
+		this.form = this.fb.group(this.ctrl)
+		this.form.valueChanges.subscribe(
+			(res) =>
+			{ console.log(res); }
+		)
+	}
+
+	onSubmit() {
+		var data = this.form.value;
+		this.sectionStore.updateComponent(this.section.component._id, data ,this.section._id);
+
+	}
 }
