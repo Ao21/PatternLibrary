dc45f271286bfc69f4dfbed4744e69aac6a6cbe2
Ronan Brett <Ronan.Brett@theaa.ie>
2015-12-17 17:23:13 +0000

    Update Edit Bar to incorporate new extend bar

--- src/app/components/shared/edit_bar/edit_bar.ts
+++ src/app/components/shared/edit_bar/edit_bar.ts
@@ -1,7 +1,8 @@
-import { Component, View } from 'angular2/core';
+import { Component, View, EventEmitter } from 'angular2/core';
 import { Tab, TabsGroup} from './../../base/tab/tab_modules.ts';
 import {AddPattern} from './../add_pattern/add_pattern.ts';
 import {AddComponent} from './../add_component/add_component';
+
 let template = require('./edit_bar.html');
 let styles = require('./edit_bar.scss');
 
@@ -11,7 +12,8 @@ let styles = require('./edit_bar.scss');
 */
 @Component({
 	selector: 'edit-bar',
-	inputs: ['index','location'],
+	inputs: ['index', 'location', 'sectionComponent:section-component'],
+	outputs: ['onIsExtended'],
 	host: {
 		'[class.c-extend-bar]':'true',
 		'[class.isExtended]': 'isExtended'
@@ -23,14 +25,16 @@ let styles = require('./edit_bar.scss');
 	directives: [AddPattern, TabsGroup, Tab, AddComponent]
 })	    
 export class EditBar {
+	onIsExtended: EventEmitter<any> = new EventEmitter();
 	location: any;
+	sectionComponent: any;
 	isExtended: boolean = false;
 	index: any;
 	constructor() {
 		
 	}
 	toggleBar(toggle: boolean) {
-
 		this.isExtended = toggle;
+		this.onIsExtended.emit(toggle);
 	}
 }
